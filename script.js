e413731fa0ad0a8e01e3b860b0495c926ca03e1a
King LEE <a0988698776@gmail.com>
2021-01-12 15:57:16 +0800

    Using Strict Mode

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let hasDeiversLicense = false
+const passTest = true
+
+if (passTest) hasDeiversLicense = true
+if (hasDeiversLicense) console.log('I can drive')
+
+// const interface = 'Audio'
+// const private = 543
+// const if = 23;
