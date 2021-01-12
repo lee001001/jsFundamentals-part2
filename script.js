1678cda7e4d7c05e2428e36c2295e2f4039254d8
King LEE <a0988698776@gmail.com>
2021-01-12 16:45:14 +0800

    Function express vs Function declaration

--- script.js
+++ script.js
@@ -11,25 +11,40 @@ if (hasDeiversLicense) console.log('I can drive')
 // const if = 23;
 */
 
-function logger() {
-  console.log('My name is Jonas')
+// function logger() {
+//   console.log('My name is Jonas')
+// }
+
+// // calling / running / invoking function
+// logger()
+// logger()
+// logger()
+
+// function fruitProcessor(apples, oranges) {
+//   // console.log(apples, oranges)
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//   return juice
+// }
+
+// // Juice 需要建立一個變數來儲存
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+// // console.log(fruitProcessor(5, 0))
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice)
+
+// Function declaration
+function calcAge1 (birthYear) {
+  return 2037 - birthYear
 }
 
-// calling / running / invoking function
-logger()
-logger()
-logger()
+const age1 = calcAge1(1991)
 
-function fruitProcessor(apples, oranges) {
-  // console.log(apples, oranges)
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-  return juice
+// Function expression : function is value
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear
 }
+const age2 = calcAge2(1991)
 
-// Juice 需要建立一個變數來儲存
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
-// console.log(fruitProcessor(5, 0))
-
-const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
+console.log(age1, age2)
