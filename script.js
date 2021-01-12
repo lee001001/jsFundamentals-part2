ff168c60b187359407dd42c307803d0f95686c2e
King LEE <a0988698776@gmail.com>
2021-01-12 17:09:00 +0800

    Arrow Function without return if only have one paremeters but if it have two or more paremeters  should be have return

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ if (hasDeiversLicense) console.log('I can drive')
 // console.log(appleOrangeJuice)
 
 // Function declaration
-function calcAge1 (birthYear) {
+function calcAge1(birthYear) {
   return 2037 - birthYear
 }
 
@@ -45,6 +45,16 @@ const age1 = calcAge1(1991)
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear
 }
-const age2 = calcAge2(1991)
 
-console.log(age1, age2)
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(1991)
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear
+  const retirmnet = 65 - age
+  // return retirmnet
+  return `${firstName} retires in ${retirmnet} years`
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'))
