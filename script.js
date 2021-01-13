0771e4b4a370f9acc0211a661695b7d72b75cc2d
King LEE <a0988698776@gmail.com>
2021-01-13 17:48:17 +0800

    Dot notion

--- script.js
+++ script.js
@@ -76,3 +76,36 @@ function fruitProcessor (apples, oranges) {
 
 console.log(fruitProcessor(2, 3))
 */
+
+// Object
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+}
+console.log(jonas)
+
+const nameKey = 'Name'
+console.log('first' + nameKey)
+console.log('last' + nameKey)
+
+// console.log(jonas. 'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName,age,job,and friends')
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn])
+} else {
+  console.log('Wrong request! Choose between firstName, lastName,age,job,and friends')
+}
+
+jonas.location = 'Portugal'
+jonas.twitter = '@jonasschmedtman'
+console.log(jonas)
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Micel"
+jonas.bestFriend = 'Mical'
+console.log(`${jonas.fristName} has${jonas.friends.length} friends ,and his best friend is called${jonas.friends[0]}`)
