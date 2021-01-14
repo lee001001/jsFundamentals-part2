48dadf3b2f185bb6f3b85b13cf19a98f4e88010a
King LEE <a0988698776@gmail.com>
2021-01-14 22:36:28 +0800

    CodingChange#4-use array method to calc

--- script.js
+++ script.js
@@ -272,6 +272,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
   }
 }
 */
+/*
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weight repetition ${rep} 🏋️‍♀️`)
 }
@@ -287,3 +288,57 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1
   if (dice === 6) console.log('Loop is about to end...')
 }
+*/
+/*
+Let's improve Steven's trip calcculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill valuse
+2. Create empty arrays for  the tips and the totals ('tips and 'total')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values(bill + tip) for every bill value in the bills array.
+Use a for loop to perform the 10 calculation!
+
+Test DATA: 22, 295, 176, 440, 37, 105, 10, 1100,86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays
+
+4. BONUS: Write a function 'calcAverage' with takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge(we haven't done this befoe)! Here si how to solve it:
+  4.1: First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have ask values added together.
+  4.2: To calculate the average, divide the sum you calculated before the length of the array(because that's the number of element)
+  4.3 Call the function with the 'totals' array
+*/
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1110, 86, 52]
+const tips = []
+const totals = []
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+}
+const calcSum = function (tips, bill) {
+  return tips + bill
+}
+const calcAverage = function (arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length
+}
+
+for (let i = 0; i < bills.length; i++) {
+  // const tips = []
+  // const totals = []
+  tips.push(calcTip(bills[i]))
+  totals.push(calcSum(bills[i], tips[i]))
+
+  // for (let i = 0; i < bills.length; i++) {
+  //   totals[i] = totals.push(tips[i] + bills[i])
+  // }
+  console.log(`tips ${i} is ${tips} bills ${i} is ${bills[i]}, and total(${i}) is ${totals[i]} and the Average is （${calcAverage(totals)}）`)
+}
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]]
+
+// console.log(`You total value is ${bill + tip}`)
+console.log(`the Average is （${calcAverage(totals)}）`)
